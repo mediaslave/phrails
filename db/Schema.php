e957aaca6fc9420d2d1bd7fcdcedf676956c0a38
Dave Kerschner <docgnome@docgno.me>
2011-01-19 16:25:11 -0800

    Fixed Schema relationships for joins

--- db/Schema.php
+++ db/Schema.php
@@ -110,10 +110,12 @@ class Schema
 	 **/
 	public function className($table, $is_global_class=false)
 	{
-		$namespace = '';
-		if(!$is_global_class){
-			$namespace = PR_APPLICATION_NAMESPACE . '\App\Models\\';
-		}
+    $namespace = PR_APPLICATION_NAMESPACE . '\App\Models\\';
+		if($is_global_class){
+      $pieces = explode('\\', $table);
+      $table = array_pop($pieces);
+      $namespace = implode('\\', $pieces) . '\\';
+    }
 		return $this->prop(Inflections::underscore($table) . '_id')
 					->addOption(array('table' => Inflections::tableize($table)), 'table')
 					->addOption(array('klass' => $namespace . $table), 'klass');
