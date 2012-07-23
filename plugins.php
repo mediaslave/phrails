a0a5b82a2ffbaf6718bbe344b02e8b80a21b6f44
Justin <justin@mediaslave.net>
2012-07-23 12:13:46 -0700

    Loading plugin file ini's has been cleaned up and made clearer.

--- plugins.php
+++ plugins.php
@@ -4,20 +4,22 @@
  * @package framework
  */
 //Load any plugin ini files and store them in pr-plugin-$filename
-$plugin_inis = "config/*.ini";
+$plugin_inis = Registry::get('pr-real-install-path') . "/config/*.ini";
 foreach (glob($plugin_inis) as $filename) {
-	if($filename != 'config/database.ini'){
-		$ini = parse_ini_file($filename, true);
-		$rkey = str_replace('config/', '', $filename);
-		$rkey = str_replace('.ini', '', $rkey);
-		$obj = new stdClass;
+	if($filename == Registry::get('pr-real-install-path') . '/config/database.ini'){
+		continue;
+	}	
+	//otherwise process the ini.
+	$ini = parse_ini_file($filename, true);
+	$rkey = str_replace(Registry::get('pr-real-install-path') . '/config/', '', $filename);
+	$rkey = str_replace('.ini', '', $rkey);
+	$obj = new stdClass;
 
-		if(isset($ini[Registry::get('pr-environment')])){
-			$obj = (object) $ini[Registry::get('pr-environment')];
-		}
-		if(isset($ini['global'])){
-			$obj->{'global'} = (object) $ini['global'];
-		}
-		Registry::set('pr-plugin-' . $rkey, $obj);
+	if(isset($ini[Registry::get('pr-environment')])){
+		$obj = (object) $ini[Registry::get('pr-environment')];
 	}
+	if(isset($ini['global'])){
+		$obj->{'global'} = (object) $ini['global'];
+	}
+	Registry::set('pr-plugin-' . $rkey, $obj);
 }
